159bd6298b34bf5a4ab3f579b9d8efe4cc010343
pkdism <pawan.dwivedi94@gmail.com>
2020-04-24 20:34:58 +0530

    day 24

--- 30-day-leetcoding-challenge/d24-lru-cache.py
+++ 30-day-leetcoding-challenge/d24-lru-cache.py
@@ -1,73 +1,37 @@
-from collections import defaultdict
+"""
+Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+"""
+
+
+from collections import OrderedDict
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.dic = defaultdict()
-        self.priority = defaultdict()
-        self.p_value = 0
-        self.lowest_value = 0
-        
-    def prnt(self):
-        print("dictionary", self.dic)
-        print("priority", self.priority)
-        print("p_value", self.p_value)
-        print("lowest_point", self.lowest_value)
-        print("")
+        self.dic = OrderedDict()
 
     def get(self, key: int) -> int:
         
-        # print("Under get -------------------")
-        # print("get key", key)
-        
         if key in self.dic.keys():
-            self.p_value += 1
-            if self.p_value > self.capacity:
-                self.lowest_value += 1
-            self.priority[self.p_value] = key
-            
-            # print("Under if --------")
-            # self.prnt()
-            
+            self.dic.move_to_end(key)
             return self.dic[key]
-        
         else:
-            
-            # print("Under else --------")
-            # self.prnt()
-            
             return -1
 
     def put(self, key: int, value: int) -> None:
-        
-        # print("Under put -------------------")
-        # print("put key", key, " value", value)
-        
-        if key in self.dic.keys():
-            self.capacity += 1
-        
-        self.dic[key] = value
-        
-        if self.p_value > self.capacity:
-            
-            self.lowest_value += 1
-            x = self.dic.pop(self.priority[self.lowest_value])
-            self.p_value += 1            
-            self.priority[self.p_value] = key
-            
-            
-            # print("Under if --------")
-            # self.prnt()
-            
+        if key not in self.dic.keys():
+            if len(self.dic) >= self.capacity:
+                self.dic.popitem(last = False)
+            self.dic[key] = value
         else:
-            
-            self.p_value += 1
-            self.priority[self.p_value] = key
-            
-            # print("Under else --------")
-            # self.prnt()
-
+            self.dic.move_to_end(key)
+            self.dic[key] = value
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
