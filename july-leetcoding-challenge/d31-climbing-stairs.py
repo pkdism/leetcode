b31903e505b75b6a372a52587a5ce163264ec0d0
pkdism <pawan.dwivedi94@gmail.com>
2020-07-31 17:56:02 +0530

    day 31

---
+++ july-leetcoding-challenge/d31-climbing-stairs.py
@@ -0,0 +1,18 @@
+"""
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+"""
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = 1
+        b = 1
+        res = 1
+        
+        for i in range(2, n+1):
+            res = a + b
+            a = b
+            b = res
+        
+        return res
